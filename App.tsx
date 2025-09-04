51486188f1fb539183175cc9336703e718077808
Vanessa Baldin <vanbaldin08@gmail.com>
2025-09-04 10:02:08 -0300

    adiciona estilos nas views botoes e rodape

--- App.tsx
+++ App.tsx
@@ -35,13 +35,21 @@ const estilos = StyleSheet.create({
   viewLogo: {
     backgroundColor: "#a5d6a7",
     flex: 3,
+    justifyContent: "flex-end",
+    alignItems: "center",
   },
   viewBotoes: {
     backgroundColor: "#ffcc80",
     flex: 2,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
   },
   viewRodape: {
     backgroundColor: "#ef9a9a",
     flex: 0.5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
 });
