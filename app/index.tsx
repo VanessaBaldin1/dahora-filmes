d5f69c37363006ec6e8b291a3f2e4234f537d7cb
Vanessa Baldin <vanbaldin08@gmail.com>
2025-09-23 08:32:27 -0300

    Cria tela buscar

--- app/index.tsx
+++ app/index.tsx
@@ -17,10 +17,10 @@ export default function index() {
           <Text style={estilos.tituloApp}>Dá Hora Filmes </Text>
         </View>
         <View style={estilos.viewBotoes}>
-          <Link href="/buscar" asChild>
+          <Link href="/busca" asChild>
             <Pressable style={estilos.botaoInicial}>
               <Ionicons name="search" size={18} color="white" />
-              <Text style={estilos.textoBotao}> Buscar Filmes</Text>
+              <Text style={estilos.textoBotao}> Busca Filmes</Text>
             </Pressable>
           </Link>
           <Link href="/favoritos" asChild>
